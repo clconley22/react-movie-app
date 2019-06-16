f12b0707e7e1c966576a2165875d9bf624e28259
Christina Conley <clconley22@gmail.com>
2019-06-16 14:36:18 -0500

    got different api to work

--- src/components/moviesList.jsx
+++ src/components/moviesList.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
+const API_URL = "https://jsonplaceholder.typicode.com/users";
+// "https://api.themoviedb.org/3/movie/popular?api_key=30ade5dac6f8d31a4b3bfc2083fcb2d5&language=en-US&page=1";
 
 class MoviesList extends Component {
   state = {
@@ -7,26 +9,34 @@ class MoviesList extends Component {
   };
 
   componentDidMount() {
+    const url = `${API_URL}`;
     axios
-      .get(
-        `https://api.themoviedb.org/3/movie/popular?api_key=30ade5dac6f8d31a4b3bfc2083fcb2d5&language=en-US&page=1`
-      )
-      .then(res => {
-        // console.log(res.data.results[0].title);
-        this.setState({ persons: res.data });
+      .get(url)
+      .then(response => response.data)
+      .then(data => {
+        this.setState({ persons: data });
         console.log(this.state.persons);
       });
   }
 
   render() {
     return (
-      <ul>
-        {this.state.persons.map(person => {
-          <li> {(this.persons = person.name)} </li>;
-        })}
-      </ul>
+      <div className="container">
+        <div className="col-xs-8">
+          <h1>React Axios Example</h1>
+          {this.state.persons.map(person => (
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">{person.name}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">
+                  {person.email}
+                </h6>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     );
-    console.log(this.state.persons);
   }
 }
 
