6f1447ac001d572a3bb003cfdaca2a226f8dfd41
Christina Conley <clconley22@gmail.com>
2019-06-16 17:21:39 -0500

    got movie api to show titles and description...working on images

--- src/components/moviesList.jsx
+++ src/components/moviesList.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import axios from "axios";
-const API_URL = "https://jsonplaceholder.typicode.com/users";
-// "https://api.themoviedb.org/3/movie/popular?api_key=30ade5dac6f8d31a4b3bfc2083fcb2d5&language=en-US&page=1";
+const IMAGE_URL = "https://image.tmdb.org/t/p/w500/";
+
+const API_URL =
+  "https://api.themoviedb.org/3/movie/popular?api_key=30ade5dac6f8d31a4b3bfc2083fcb2d5&language=en-US&page=1";
 
 class MoviesList extends Component {
-  state = {
-    persons: []
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: []
+    };
+  }
 
   componentDidMount() {
     const url = `${API_URL}`;
@@ -14,22 +19,28 @@ class MoviesList extends Component {
       .get(url)
       .then(response => response.data)
       .then(data => {
-        this.setState({ persons: data });
-        console.log(this.state.persons);
+        this.setState({ movies: data.results });
+        console.log(this.state.movies);
       });
   }
 
   render() {
+    // const image = "`${IMAGE_URL}`" + {movie.backdrop_path}
+    // const movieTitle = this.state.movies.results[0].title;
     return (
       <div className="container">
         <div className="col-xs-8">
-          <h1>React Axios Example</h1>
-          {this.state.persons.map(person => (
+          <h1>Popular Movies</h1>
+          {this.state.movies.map(movie => (
             <div className="card">
               <div className="card-body">
-                <h5 className="card-title">{person.name}</h5>
+                <h5 className="card-title">{movie.title}</h5>
+                <img
+                  src="`${API_URL}/{movies.backdrop_path}`"
+                  alt="movie image"
+                />
                 <h6 className="card-subtitle mb-2 text-muted">
-                  {person.email}
+                  {movie.overview}
                 </h6>
               </div>
             </div>
